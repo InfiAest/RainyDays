be701d80fe1339d9896cce4aa71baea31f6f9e17
Charlotte Lucas <71286689+InfiAest@users.noreply.github.com>
2021-03-17 19:59:39 +0100

    formvalidation

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,6 +1,6 @@
-const addToCartModal = document.getElementById("add-to-cart-modal");
+const addToCartModal = document.querySelector("#add-to-cart-modal");
 
-const addButton = document.getElementById("add-to-cart-button");
+const addButton = document.querySelector("#add-to-cart-button");
 
 addButton.onclick = function() {
     addToCartModal.style.display = "flex";
---
+++ js/checkoutValidation.js
@@ -0,0 +1,186 @@
+const form = document.querySelector("#checkout-form");
+const completeButton = document.querySelector("#complete-order-button")
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const telephone = document.querySelector("#telephone");
+const telephoneError = document.querySelector("#telephoneError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const streetName = document.querySelector("#streetName");
+const streetNameError = document.querySelector("#streetNameError");
+const streetNumber = document.querySelector("#streetNumber");
+const streetNumberError = document.querySelector("#streetNumberError");
+const cityName = document.querySelector("#cityName");
+const cityNameError = document.querySelector("#cityNameError");
+const zipcode = document.querySelector("#zipcode");
+const zipcodeError = document.querySelector("#zipcodeError");
+const cardName = document.querySelector("#cardName");
+const cardNameError = document.querySelector("#cardNameError");
+const cardNumber = document.querySelector("#cardNumber");
+const cardNumberError = document.querySelector("#cardNumberError");
+const expDateMonth = document.querySelector("#expDate");
+const expDateMonthError = document.querySelector("#expDateError");
+const cvcNumber = document.querySelector("#cvcNumber");
+const cvcNumberError = document.querySelector("#cvcNumberError");
+const newStreetName = document.querySelector("#newStreetName");
+const newStreetNameError = document.querySelector("#newStreetNameError");
+const newStreetNumber = document.querySelector("#newStreetNumber");
+const newStreetNumberError = document.querySelector("#newStreetNumberError");
+const newCityName = document.querySelector("#newCityName");
+const newCityNameError = document.querySelector("#newCityNameError");
+const newZipcode = document.querySelector("#newZipcode");
+const newZipcodeError = document.querySelector("#newZipcodeError");
+const formValidation = document.querySelector(".form-validation");
+const newAddressForm = document.querySelector(".new-address-form");
+var validateNewAddress = true;
+
+function validateForm(event) {
+    event.preventDefault();
+
+    var formIsValid = true;
+
+    if (checkLength(firstName.value, 1) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(lastName.value, 1) === true) {
+        lastNameError.style.display = "none";
+    } else {
+        lastNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(telephone.value, 7) === true) {
+        telephoneError.style.display = "none";
+    } else {
+        telephoneError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(streetName.value, 6) === true) {
+        streetNameError.style.display = "none";
+    } else {
+        streetNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(streetNumber.value, 0) === true) {
+        streetNumberError.style.display = "none";
+    } else {
+        streetNumberError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(cityName.value, 3) === true) {
+        cityNameError.style.display = "none";
+    } else {
+        cityNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(zipcode.value, 3) === true) {
+        zipcodeError.style.display = "none";
+    } else {
+        zipcodeError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(cardName.value, 8) === true) {
+        cardNameError.style.display = "none";
+    } else {
+        cardNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(cardNumber.value, 11) === true) {
+        cardNumberError.style.display = "none";
+    } else {
+        cardNumberError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(cvcNumber.value, 2) === true) {
+        cvcNumberError.style.display = "none";
+    } else {
+        cvcNumberError.style.display = "block";
+        formIsValid = false;
+    }
+
+if (validateNewAddress === true) { 
+    if (checkLength(newStreetName.value, 6) === true) {
+        newStreetNameError.style.display = "none";
+    } else {
+        newStreetNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(newStreetNumber.value, 0) === true) {
+        newStreetNumberError.style.display = "none";
+    } else {
+        newStreetNumberError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(newCityName.value, 3) === true) {
+        newCityNameError.style.display = "none";
+    } else {
+        newCityNameError.style.display = "block";
+        formIsValid = false;
+    }
+
+    if (checkLength(newZipcode.value, 3) === true) {
+        newZipcodeError.style.display = "none";
+    } else {
+        newZipcodeError.style.display = "block";
+        formIsValid = false;
+    }
+}
+
+    if (formIsValid === true) {
+        formValidation.style.display = "block";
+        completeButton.innerHTML = "Complete Order";
+        completeButton.onclick = function() {
+            location.href = "checkout-sucess.html";
+        }
+    }
+}
+
+function newAddress(){
+    newAddressForm.style.display = "block";
+    validateNewAddress = true;
+}
+function sameAddress(){
+    newAddressForm.style.display = "none";
+    validateNewAddress = false;
+}
+
+
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
