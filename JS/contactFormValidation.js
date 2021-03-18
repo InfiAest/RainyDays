a0727e3f4ca4eee897cb8ab8a160c844fa81971a
Charlotte Lucas <71286689+InfiAest@users.noreply.github.com>
2021-03-18 20:58:14 +0100

    contact form finished

--- JS/contactFormValidation.js
+++ JS/contactFormValidation.js
@@ -5,7 +5,14 @@ const subject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subjectError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
+const message = document.querySelector("textarea");
+const messageError = document.querySelector("#messageError");
 const validationMessage = document.querySelector(".validation-message");
+const nameSection = document.querySelector(".name-section");
+const emailSection = document.querySelector(".email-section");
+const subjectSection = document.querySelector(".subject-section");
+const messageSection = document.querySelector(".message-section");
+const button = document.querySelector(".button");
 
 function validateForm(event) {
     event.preventDefault();
@@ -33,9 +40,20 @@ function validateForm(event) {
         formIsValid = false;
     }
 
+    if (checkLength(message.value, 29) === true) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+        formIsValid = false;
+    }
+
     if (formIsValid === true) {
+        nameSection.style.display = "none";
+        emailSection.style.display = "none";
+        subjectSection.style.display = "none";
+        messageSection.style.display = "none";
+        button.style.display = "none";
         validationMessage.style.display = "block";
-        console.log("Congrats! You filled out the form correctly");
     } 
 
 
