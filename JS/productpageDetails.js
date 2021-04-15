f7651ca15945400f8f214c35d6ca3a329aed4676
Charlotte Lucas <71286689+InfiAest@users.noreply.github.com>
2021-04-15 22:54:04 +0200

    .

--- JS/productpageDetails.js
+++ JS/productpageDetails.js
@@ -27,6 +27,11 @@ getProductDetails();
 
 function createProductDetails(details) {
 
+    console.log(details.name.replace(/\s/g,'').toLowerCase());
+
+
+
+
     detailsContainer.innerHTML += `<section class="details-section">
                                         <div class="title-price-container">
                                             <h1 class="jacket-name">${details.name}</h1>
@@ -45,26 +50,26 @@ function createProductDetails(details) {
                                             ${details.attributes[0].name}:
                                                 <div class="radio-colours">
                                                     <input type="radio" name="colour" id="mahoganyRed" value="red">
-                                                    <label for="mahoganyRed" class="radio-label-colour"><i class="fas fa-circle winter-shell-swatch-1"></i>Mahogany Red</label>
+                                                    <label for="mahoganyRed" class="radio-label-colour"><i class="fas fa-circle ${details.name.replace(/\s/g,'-').toLowerCase()}-swatch-1"></i>${details.attributes[0].terms[0].name}</label>
                                                     <input type="radio" name="colour" id="mossGreen" value="green">                                
-                                                    <label for="mossGreen" class="radio-label-colour"><i class="fas fa-circle winter-shell-swatch-2"></i>Moss Green</label>
+                                                    <label for="mossGreen" class="radio-label-colour"><i class="fas fa-circle ${details.name.replace(/\s/g,'-').toLowerCase()}-swatch-2"></i>${details.attributes[0].terms[1].name}</label>
                                                     <input type="radio" name="colour" id="moonGrey" value="grey">
-                                                    <label for="moonGrey" class="radio-label-colour"><i class="fas fa-circle winter-shell-swatch-3"></i>Moon Grey</label>
+                                                    <label for="moonGrey" class="radio-label-colour"><i class="fas fa-circle ${details.name.replace(/\s/g,'-').toLowerCase()}-swatch-3"></i>${details.attributes[0].terms[2].name}</label>
                                                 </div>
                                             </label>
                                             <label>
                                             ${details.attributes[1].name}:
                                                 <div class="radio-sizes">
                                                     <input type="radio" name="size" id="xSmall" value="XS">
-                                                    <label for="xSmall" class="radio-label-size">XS</label>
+                                                    <label for="xSmall" class="radio-label-size">${details.attributes[1].terms[0].name}</label>
                                                     <input type="radio" name="size" id="small" value="S">
-                                                    <label for="small" class="radio-label-size">S</label>
+                                                    <label for="small" class="radio-label-size">${details.attributes[1].terms[1].name}</label>
                                                     <input type="radio" name="size" id="medium" value="M">
-                                                    <label for="medium" class="radio-label-size">M</label>
+                                                    <label for="medium" class="radio-label-size">${details.attributes[1].terms[2].name}</label>
                                                     <input type="radio" name="size" id="large" value="L">
-                                                    <label for="large" class="radio-label-size">L</label>
+                                                    <label for="large" class="radio-label-size">${details.attributes[1].terms[3].name}</label>
                                                     <input type="radio" name="size" id="xLarge" value="XL">
-                                                    <label for="xLarge" class="radio-label-size">XL</label>
+                                                    <label for="xLarge" class="radio-label-size">${details.attributes[1].terms[4].name}</label>
                                                 </div>
                                             </label>
                                             <label>
