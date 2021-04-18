72755d926d95087b8f690973b1b47c533e31519c
Charlotte Lucas <71286689+InfiAest@users.noreply.github.com>
2021-04-18 13:57:59 +0200

    .

--- JS/allproducts.js
+++ JS/allproducts.js
@@ -32,7 +32,7 @@ getProducts();
 
             const list = result[i];
 
-            console.log(list);
+            // console.log(list);
 
             // console.log(list.attributes[0].options[0]);
             productContainer.innerHTML += `<div class="product">
